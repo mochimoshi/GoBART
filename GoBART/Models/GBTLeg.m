4877d7cbd92c5d88f18283a23681c56918d11dae
Tim Hsieh <junting@stanford.edu>
2015-02-07 00:18:18 -0800

    Add init methods in GBTLeg, GBTTrip

--- GoBART/Models/GBTLeg.m
+++ GoBART/Models/GBTLeg.m
@@ -10,4 +10,18 @@
 
 @implementation GBTLeg
 
+- (instancetype)initLegWithAttributes:(NSDictionary *)attributeDict {
+    self = [super init];
+    if (self) {
+        self.line = attributeDict[@"line"];
+        self.origTimeMin = attributeDict[@"origTimeMin"];
+        self.origTimeDate = attributeDict[@"origTimeDate"];
+        self.destTimeMin = attributeDict[@"destTimeMin"];
+        self.destTimeDate = attributeDict[@"destTimeDate"];
+        self.departStation = attributeDict[@"departStation"];
+        self.arriveStation = attributeDict[@"departStation"];
+    }
+    return self;
+}
+
 @end
